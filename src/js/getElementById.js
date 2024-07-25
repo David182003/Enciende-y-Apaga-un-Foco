c5236de197a0c97cc0ac4a368da4452ce602ba23
David182003 <jorgeminano18@gmail.com>
2024-07-25 14:44:01 -0500

    V-0.1

---
+++ src/js/getElementById.js
@@ -0,0 +1,8 @@
+document.getElementById("demo").innerHTML = "SwitchOn";
+document.getElementById("demo").style.fontSize = "55px";
+function prenderFoco() {
+    document.getElementById('myImage').src = '../public/pic_bulbon.gif';
+  }
+function apagarFoco() {
+    document.getElementById('myImage').src = '../public/pic_bulboff.gif';
+  }
